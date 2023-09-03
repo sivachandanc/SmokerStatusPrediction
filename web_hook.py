68b299b36f4378ac24ea38deabffd8b63a59cf08
sivachandanc <sivachandan1996@gmail.com>
2023-09-03 16:16:02 -0400

    Finallized the webhook script

--- web_hook.py
+++ web_hook.py
@@ -1,19 +1,16 @@
 from flask import Flask, request, jsonify
-
 import git
 
-repo = git.Repo("./")
-origin = repo.remote(name='origin')
-origin.push()
+app = Flask(__name__)
 
-# app = Flask(__name__)
-
-# @app.route('/webhook', methods=['POST'])
-# def handle_webhook():
-#     data = request.json
-#     # You can add more processing logic here, like saving the data to a database, etc.
-#     return jsonify({'status': 'success'}), 200
+@app.route('/webhook', methods=['POST'])
+def handle_webhook():
+    repo = git.Repo("./")
+    origin = repo.remote(name='origin')
+    origin.push()
+    # You can add more processing logic here, like saving the data to a database, etc.
+    return jsonify({'status': 'success'}), 200
 
 
-# if __name__ == '__main__':
-#     app.run(debug=True, port=5000)
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
